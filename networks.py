23cb842ff2c7b818edb9866bd71fb2e909f8b5bf
Roman Koshelev <roman.koshelev@groupdocs.com>
2017-12-08 21:05:31 +0300

    trained on 5k up to 89% of acc

--- networks.py
+++ networks.py
@@ -2,6 +2,28 @@ import tensorflow as tf
 import numpy as np
 from layers import *
 
+
+def simple_conv(images, out_dim, reuse, training):
+    with tf.variable_scope("simple_conv", reuse=reuse):
+        l = batch_norm(images, training) # -> 128
+
+        l = conv2d_maxpool(l,  16) # -> 64
+        l = conv2d_maxpool(l,  32) # -> 32
+        l = conv2d_maxpool(l,  64) # -> 16
+        l = conv2d_maxpool(l, 128) # -> 8
+        l = conv2d_maxpool(l, 256) # -> 4
+
+        l = flatten(l)             # -> 1024
+
+        l = dense(l, 1024)
+        l = dense(l, 1024)
+        l = dense(l, out_dim, activation=None)
+        
+        l = normalize(l)
+        
+        return l
+
+    
 def AlexNet(images, out_dim, reuse, training):
     with tf.variable_scope("AlexNet", reuse=reuse):
         l = batch_norm(images, training) # -> 128
@@ -24,31 +46,11 @@ def AlexNet(images, out_dim, reuse, training):
 
         l = dense(l, 2048)
         l = dense(l, 2048)
-        l = dense(l, out_dim)
+        l = dense(l, out_dim, activation=None)
 
         l = normalize(l)
     
     return l
-
-def simple_conv(images, out_dim, reuse, training):
-    with tf.variable_scope("simple_conv", reuse=reuse):
-        l = batch_norm(images, training) # -> 128
-
-        l = conv2d_maxpool(l,  16) # -> 64
-        l = conv2d_maxpool(l,  32) # -> 32
-        l = conv2d_maxpool(l,  64) # -> 16
-        l = conv2d_maxpool(l, 128) # -> 8
-        l = conv2d_maxpool(l, 256) # -> 4
-
-        l = flatten(l)             # -> 1024
-
-        l = dense(l,    1024)
-        l = dense(l,    1024)
-        l = dense(l, out_dim, activation=None)
-        
-        l = normalize(l)
-        
-        return l
 
     
 def VGG16(images, out_dim, reuse, training):
@@ -71,9 +73,9 @@ def VGG16(images, out_dim, reuse, training):
         
         l = flatten(l)                  # -> 8192
 
-        l = dense(l,    2048)
-        l = dense(l,    2048)
-        l = dense(l, out_dim)
+        l = dense(l, 2048)
+        l = dense(l, 2048)
+        l = dense(l, out_dim, activation=None)
         
         l = normalize(l)
         
